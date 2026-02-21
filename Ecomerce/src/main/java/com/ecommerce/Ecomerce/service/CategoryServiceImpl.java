4f8609ce7a553e560fc178c8cb6a0a936e4ac6b9
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-02-21 21:34:01 +0600

    managing id

--- Ecomerce/src/main/java/com/ecommerce/Ecomerce/service/CategoryServiceImpl.java
+++ Ecomerce/src/main/java/com/ecommerce/Ecomerce/service/CategoryServiceImpl.java
@@ -10,14 +10,17 @@ import java.util.List;
 public class CategoryServiceImpl implements  CategoryService {
 
     private List<Category> categories= new ArrayList<>();
+    private Long nextId=1L;
 
     @Override
     public List<Category> getAllCategories() {
+
         return categories;
     }
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 }
