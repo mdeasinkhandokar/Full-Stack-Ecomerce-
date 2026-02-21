4f5dfed9bb9fe74dc158fccd7134fcbc21d86527
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-02-21 20:56:27 +0600

    fatching data

---
+++ Ecomerce/src/main/java/com/ecommerce/Ecomerce/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.ecommerce.Ecomerce.service;
+
+import com.ecommerce.Ecomerce.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+}
