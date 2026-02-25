b5030a1129fb8ff0505f86212203724243ce5bd3
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-02-25 16:48:07 +0600

    JpaRepository

---
+++ Ecomerce/src/main/java/com/ecommerce/Ecomerce/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.Ecomerce.repositories;
+
+import com.ecommerce.Ecomerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+    
+}
