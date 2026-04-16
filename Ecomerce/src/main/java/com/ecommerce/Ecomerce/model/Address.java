206d776954228e8bdc38841a6ac513a6d1c6fd7b
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-04-17 01:39:21 +0600

    address

--- Ecomerce/src/main/java/com/ecommerce/Ecomerce/model/Address.java
+++ Ecomerce/src/main/java/com/ecommerce/Ecomerce/model/Address.java
@@ -1,12 +1,22 @@
 package com.ecommerce.Ecomerce.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
 
@@ -14,5 +24,51 @@ public class Address {
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long addressId;
 
+    @NotBlank
+    @Size(min=5 , message="Street name must be atleast 5 characters")
+    private String street;
 
+
+
+    @NotBlank
+    @Size(min=5 , message="buildingName name must be atleast 5 characters")
+    private String buildingName;
+
+
+
+    @NotBlank
+    @Size(min=4 , message="City name must be atleast 5 characters")
+    private String city;
+
+
+
+    @NotBlank
+    @Size(min=2 , message="Satename must be atleast 5 characters")
+    private String state;
+
+
+    @NotBlank
+    @Size(min=5 , message="Country name must be atleast 5 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min=6 , message="Pincode must be atleast 5 characters")
+    private String pincode;
+
+
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users= new ArrayList<>();
+
+
+    public Address(Long addressId, String street, String buildingName, String city, String state, String country, String pincode) {
+        this.addressId = addressId;
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pincode = pincode;
+    }
 }
